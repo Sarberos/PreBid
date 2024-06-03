724c27c91a0fcc9414e2778eead2e99f5aad9ca0
sarberos <denismasters@mail.ru>
2024-06-03 20:49:34 +0300

    car filters final

--- src/components/Main/CarList/LeftColumn/FilterElemRed/FilterElemRed.jsx
+++ src/components/Main/CarList/LeftColumn/FilterElemRed/FilterElemRed.jsx
@@ -9,15 +9,79 @@ const FilterElemRed = ({fieldValue ,children}) => {
   const[filterOn,setFilterStat]=useState({
     status: false,
     value:'',
-  })
-  
+  })                                                    //переделать 
   const onsetFilterStat=(e)=>{
+    if(filtersArr.find(filter=>filter===e.target.innerText)){
+      console.log('Элемент существует')
+    }else{
+      setFiltersArr([...filtersArr, e.target.innerText])
+    }
+    
     setFilterStat({
       status: true,
       value:e.target.innerText});
       setPropertyStat(false)
   }
- 
+
+
+
+  const[filtersArr,setFiltersArr]=useState([])
+  const removeFilter=(filterName)=>{
+    setFiltersArr(filtersArr.filter(filter=> filter !==filterName))
+  }
+  const transFilterName=(name)=>{
+    let  transName=''
+    switch (name) {
+      case 'odometer':
+        transName='Одометр'
+        break;
+    
+      case 'year':
+        transName='Год'
+        break;
+    
+      case 'keys':
+        transName='Ключи'
+        break;
+    
+      case 'transportHighlight':
+        transName='Состояние'
+        break;
+    
+      case 'transportTransmission':
+        transName='Тип трансмисии'
+        break;
+    
+      case 'transportName':
+        transName='Название'
+        break;
+    
+      case 'transportDrive':
+        transName='Привод'
+        break;
+    
+      case 'transportFuel':
+        transName='Тип топлива'
+        break;
+    
+      case 'transportBrand':
+        transName='Марка'
+        break;
+      case 'transportModel':
+        transName='Модель'
+        break;
+      case 'transportType':
+        transName='Тип транспорта'
+        break;
+      case 'transportColor':
+        transName='Цвет'
+        break;
+      default:
+        return name;
+    }
+    return transName;
+  }
+
     return (
         <div tabIndex='0' onBlur={()=>setPropertyStat(false)} className={s.list_box_item}>
           <div className={s.list_item}>
@@ -25,11 +89,13 @@ const FilterElemRed = ({fieldValue ,children}) => {
               <div onClick={()=>setPropertyStat(!propertyOn)} className={s.select_box}>
               <div className={s.item_value}>
                 <div className={s.multi_filter}>
-                  <p className={s.filter_text}>{children}</p>
-                  <div className={filterOn.status ? `${s.choose_box} ${s.active}`:`${s.choose_box}`}>
-                    <p className={s.pop_up_filter_label}>{filterOn.value}</p>
-                    <button onClick={()=>setFilterStat({status: false,})} className={s.pop_up_filter_btn}></button>
-                  </div>
+                  <p className={s.filter_text}>{transFilterName(children)}</p>
+                  {filtersArr.map(filter=>(
+                     <div   className={filterOn.status ? `${s.choose_box} ${s.active}`:`${s.choose_box}`}>
+                    <p className={s.pop_up_filter_label} >{filter}</p>
+                    <button onClick={()=>removeFilter(filter)}className={s.pop_up_filter_btn}></button>
+                  </div> 
+                  ))}
                 </div>
               </div>
               <button  className={s.item_btn}></button>
@@ -38,7 +104,7 @@ const FilterElemRed = ({fieldValue ,children}) => {
                 <div  className={s.select_box_item}>
                 {Object.keys(fieldValue).map((fieldValueName,index)=>{
               return (<div onClick={onsetFilterStat} key={index} className={s.select_point}>
-              {typeof fieldValue[fieldValueName]==='object' ? fieldValue[fieldValueName]?.name:fieldValue[fieldValueName]}</div>)})}
+              {typeof fieldValue[fieldValueName]==='object' ? [fieldValue[fieldValueName]?.name?fieldValue[fieldValueName]?.name:fieldValue[fieldValueName]?.value]:fieldValue[fieldValueName]}</div>)})}
                   {/* <div onClick={onsetFilterStat} className={s.select_point}>Не находу </div>
                   <div className={s.select_point}>Заводится </div> */}
 
