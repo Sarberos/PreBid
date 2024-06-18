d66e46d7cf845252eeee7d6573a16bbb933ed71c
sarberos <denismasters@mail.ru>
2024-06-18 12:03:02 +0300

    added  hooks

---
+++ src/components/hooks/my-sels/useListOptions.jsx
@@ -0,0 +1,18 @@
+import { useQuery } from "@tanstack/react-query"
+import Fetching from "../../../http/api_request"
+import {useEffect} from 'react'
+
+export const useListOptions=()=>{
+
+    const {data,isLoading,isSuccess,isError}=useQuery({
+        queryKey:['sales options list'],
+        queryFn: ()=>Fetching.getListOptions(),
+        select: resp=> resp.data.content,
+    })
+    useEffect(() => {
+        data && console.log(data);
+      }, [isSuccess, data]);
+
+
+    return {data,isLoading,isSuccess,isError}
+}
